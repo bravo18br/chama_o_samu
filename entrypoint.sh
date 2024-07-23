f0766253b4c47f89ac0a78683fc92d7c86d7eaa3
bravo18br <bravo18br@gmail.com>
2024-07-23 10:59:15 -0300

    23/07

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,6 @@
 # Carrega as variáveis de ambiente do arquivo .env
 source /root/.env
 
-# Verifica se GITHUB_TOKEN está definido
-if [ -z "$GITHUB_TOKEN" ]; then
-  echo "Erro: GITHUB_TOKEN não está definido"
-  exit 1
-fi
-
-# Login no GitHub usando o token
-echo $GITHUB_TOKEN | gh auth login --with-token
-
-# Clona o repositório no diretório /var/www/html
-gh repo clone https://github.com/prefeitura-araucaria/samu-acessibilidade /var/www/html
-
 # Move o arquivo .env para o diretório clonado
 mv /root/.env /var/www/html/.env
 
@@ -25,19 +13,29 @@ fi
 
 # Define as permissões corretas
 chown -R www-data:www-data /var/www/html
-chmod -R 775 /var/www/html/storage
-chmod -R 775 /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/database
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/database
+chown -R www-data:www-data /var/www/html/storage/logs
+chown -R www-data:www-data /var/www/html/bootstrap/cache
 
-#Atualizar dependências do projeto
+chmod -R 775 /var/www/html
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/database
+chmod -R 775 /var/www/html/storage/logs
+chmod -R 775 /var/www/html/bootstrap/cache
+
+# Atualizar dependências do projeto
 cd /var/www/html
-composer update
-php artisan migrate
-php artisan db:seed
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Executa os comandos do Laravel
+php artisan migrate --force
+php artisan db:seed --force
 php artisan storage:link
 php artisan view:clear
-php artisan config:cache
-php artisan key:generate
+php artisan config:clear
+# php artisan config:cache // ativar isso em produção apenas
+php artisan key:generate --force
 
 # Executa o comando original do contêiner
 exec "$@"
