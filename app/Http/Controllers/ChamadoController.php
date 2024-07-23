fe289aa4ec990d36c626b29583992db09bfc709b
bravo18br <bravo18br@gmail.com>
2024-07-23 14:47:36 -0300

    23/07

--- app/Http/Controllers/ChamadoController.php
+++ app/Http/Controllers/ChamadoController.php
@@ -134,7 +134,7 @@ class ChamadoController extends Controller
 
         if ($request->hasFile('fotos')) {
             foreach ($request->file('fotos') as $foto) {
-                $path = $foto->store('fotos', 'public');
+                $path = $foto->store('fotos', 'local');
                 $url = Storage::url($path);
                 $foto = Foto::create([
                     'chamado_id' => $chamado->id,
